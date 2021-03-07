6b73755524d590b20aa5260e7039c8f5f763a257
Sofi <26.12.13.sofia@gmail.com>
2021-03-07 15:41:07 +0100

    21

---
+++ intel.js
@@ -0,0 +1,15 @@
+new Swiper('.image-slider', {
+  //cтрелки 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  //
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+
+})
