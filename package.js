cba063781d5aaec420584c37e3c1d8b94a94ae3f
Sofja <26.12.13.sofia@gmail.com>
2021-04-12 11:12:15 +0300

    ..

--- package.js
+++ package.js
@@ -78,42 +78,7 @@ $(document).ready(function () {
 
 
 //slider2
-var swiper = new Swiper('.swiper-container.swiper-one', {
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  
-  autoplay: {
-    delay: 7000,
-    disableOnInteraction: false,
-  },
-  
-});
 
 
 
-//
-function pagination() {
-  var offset = $(document).scrollTop();
-  var windowHeight = $(window).height();
-  var $body = $('body');
-  var padding = .75;
-  var pages = Object.keys($('.page')).filter((section) => Number(section) + 1).map(section => Number(section) + 1)
 
-  pages.map((page) => {
-    if (offset > (windowHeight * (page - 2 + padding))) {
-       $body.removeClass().addClass("page-" + page);
-    }
-  });
-};
-
-pagination();
-
-$(document).on('scroll', pagination);
-
-$(document).on('click', 'a[href^="#"]', function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
